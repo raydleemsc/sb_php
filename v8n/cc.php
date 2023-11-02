53103c26e2968712e620560fec5d3e76344ca3a9
Ray <raydlee.mobile@gmail.com>
2023-11-02 17:30:07 +0700

    tweak for debug to mask debugging ntoes

--- v8n/cc.php
+++ v8n/cc.php
@@ -15,7 +15,7 @@ function cc_validate($debug=false){
                     $vk=substr($v,0,$v_colon);
                     $vv=substr($v,$v_colon+1);
                 }
-                error_log("CCV:Setting cookie, vk=$vk, vv=$vv");
+                if($debug) error_log("CCV:Setting cookie, vk=$vk, vv=$vv");
                 setcookie($vk,$vv);
                 header("Refresh:0; url=/");
                 continue;
@@ -28,7 +28,7 @@ function cc_validate($debug=false){
         // 2. check for admin, if found, case command
         $admin=check_rights("admin",$debug=$debug);
         if($admin){
-            error_log( "CCV:Admin granted");
+            if($debug) error_log( "CCV:Admin granted");
             $oc_array=array("open","close");
             $req_key="";
             foreach($oc_array as $oc_key){
@@ -40,7 +40,7 @@ function cc_validate($debug=false){
 
             if($req_key){
                 $new_status=$req_key;
-                error_log( "CCV:New status = $new_status");
+                if($debug) error_log( "CCV:New status = $new_status");
                 set_system_status($new_status,$status_filename,$debug);
             }
         } else {
@@ -50,30 +50,30 @@ function cc_validate($debug=false){
         // if either admin or access approved
         if($admin || $access){
             // permit access
-            error_log( "CCV:Access granted");
+            if($debug) error_log( "CCV:Access granted");
             return true;
         } else {
-            clear_cookies();
+            clear_cookies($debug);
         }
     }
     // 4. check for site status, if open, create cookie and add to access list
     $status=get_system_status($status_filename,$debug);
-    error_log( "CCV:System status=$status");
+    if($debug) error_log( "CCV:System status=$status");
     if(in_array($status,array("open"))){
-        error_log( "CCV:Generate cookie");
+        if($debug) error_log( "CCV:Generate cookie");
         $new_cookie="access_".(string)random_int(1698733138,time());
-        error_log( "CCV:Add cookie=$new_cookie to whitelist");
+        if($debug) error_log( "CCV:Add cookie=$new_cookie to whitelist");
         $fp = fopen(ACCESS_FILE, 'a');
         fwrite($fp, $new_cookie.PHP_EOL);
         fclose($fp);
-        error_log( "CCV:Set client cookie=$new_cookie");
+        if($debug) error_log( "CCV:Set client cookie=$new_cookie");
         $try_cookie=false;
         $try_cookie=setcookie($new_cookie,$new_cookie);
         if(!$try_cookie) header("Refresh:0; url=/?setcookie=access_cookie:$new_cookie");
-        error_log( "CCV:Access granted");
+        if($debug) error_log( "CCV:Access granted");
         return true;
     } else {
-        error_log( "CCV:Access denied");
+        if($debug) error_log( "CCV:Access denied");
     }
     // 4. deny access
     return false;
--- v8n/cc_utils.php
+++ v8n/cc_utils.php
@@ -23,9 +23,9 @@ function get_file2arr($filename,$debug=false){
     $whitelist = array();
     if(file_exists($filename)) {
         $whitelist = array_map('trim',explode("\n",file_get_contents($filename)));
-        error_log( "GFA:$filename=". print_r($whitelist,true));
+        if($debug) error_log( "GFA:$filename=". print_r($whitelist,true));
     } else {
-        error_log("GFA:$filename=not found");
+        if($debug) error_log("GFA:$filename=not found");
     }
     return $whitelist;
 }
@@ -34,24 +34,24 @@ function check_rights($level="",$debug=false){
     $approved=false;
     $file_array=array("admin"=>ADMIN_FILE,"access"=>ACCESS_FILE);
     if(array_key_exists($level,$file_array)){
-        error_log( "CR:checking for $level rights");
+        if($debug) error_log( "CR:checking for $level rights");
         $level_list=get_file2arr($file_array[$level],$debug=$debug);
         $cookie_index=0;
-        while(!$approved && $cookie_index<count($_COOKIE)){
+        while($approved==false && $cookie_index<count($_COOKIE)){
             $ck_item=assoc_index($_COOKIE,$cookie_index);
-            error_log( "CR:ck_item=$ck_item");
-            if(in_array($ck_item,$level_list)) $approved=true;
-            error_log( "CR:approved=$approved");
+            if($debug) error_log( "CR:ck_item=$ck_item");
+            if(in_array($ck_item,$level_list)) $approved=$ck_item;
+            if($debug) error_log( "CR:approved=$approved");
             $cookie_index++;
         }
     }
     return $approved;
 }
 
-function clear_cookies(){
-    error_log("CC:Clearing all cookies");
+function clear_cookies($debug=false){
+    if($debug) error_log("CC:Clearing all cookies");
     foreach($_COOKIE as $k => $v){
-        error_log("CC:Clearing $k => $v");
+        if($debug) error_log("CC:Clearing $k => $v");
         setcookie($k,$v,time()-3600);
     }
 }
@@ -59,7 +59,7 @@ function clear_cookies(){
 function set_system_status($status,$status_filename,$debug=false){
     if($status=="") $status="close";
     if($status_filename=="") $status_filename=STATUS_FILE;
-    error_log( "SSS:Setting system status (in $status_filename) to $status");
+    if($debug) error_log( "SSS:Setting system status (in $status_filename) to $status");
     // echo "SSS:Setting system status (in $status_filename) to $status";
     file_put_contents($status_filename,$status);
 }
@@ -67,21 +67,76 @@ function set_system_status($status,$status_filename,$debug=false){
 function get_system_status($status_filename,$debug=false){
     if($status_filename=="") $status_filename=STATUS_FILE;
     $status="";
-    error_log( "GSS:Getting system status from $status_filename");
+    if($debug) error_log( "GSS:Getting system status from $status_filename");
     if(!file_exists($status_filename)){
         // create system status file as closed
         $status="close";
         // set_system_status($status,$status_filename=$status_filename,$debug=$debug);
-        error_log( "GSS:Setting system status (in $status_filename) to $status");
+        if($debug) error_log( "GSS:Setting system status (in $status_filename) to $status");
         // echo "GSS:Setting system status (in $status_filename) to $status";
         set_system_status($status,$status_filename,$debug);
     }
     if($status==""){
         $status=get_file2arr($status_filename,$debug)[0];
-        error_log("GSS:Current system status (in $status_filename)=".print_r($status,true));
+        if($debug) error_log("GSS:Current system status (in $status_filename)=".print_r($status,true));
     }
     return $status;
 }
 
+function set_admin($debug=false){
+
+    // Dangerous to leave unsecured
+    // 1698733138 is used as it is the base time for generation of this module
+    // It is also the lower bound for the randomly generated access cookies
+
+    $set_file=false;
+    $set_cookie=false;
+    $admin_cookie="";
+    foreach($_COOKIE as $k => $v){
+        if($k==$v && substr($k,0,6)=="admin_") {
+            $admin_cookie=$k;
+            if($debug) error_log("SA:Found admin_cookie in cookies - $admin_cookie");
+            $set_cookie=true;
+            break;
+        }
+    }
+    if(!$admin_cookie) {
+        $admin_cookie="admin_".(string)random_int(1698733138,time());
+        if($debug) error_log("SA:Set new admin_cookie=$admin_cookie");
+    }
+
+    $af_contents=get_file2arr(ADMIN_FILE);
+    foreach($af_contents as $af_item){
+        if($admin_cookie==$af_item) $set_file=true;
+    }
+
+    if(!$set_file){
+        $filemode=file_exists(ADMIN_FILE)?'a':'w';
+        // if(!file_exists(ADMIN_FILE)) $filemode='w';
+        if($debug) error_log("SA:filemode=$filemode");
+        // error_log("SA:file_exists(ADMIN_FILE)=".(string)file_exists(ADMIN_FILE));
+        $fp = fopen(ADMIN_FILE, file_exists(ADMIN_FILE)?'a':'w');
+        if($fp){
+            fwrite($fp, $admin_cookie.PHP_EOL);
+            fclose($fp);
+        }else{
+            if($debug) error_log("SA:ERROR:Write to ".ADMIN_FILE." not possible");
+        }
+    } else error_log("SA:Cookie $admin_cookie already found in admin file.");
+    if(!$set_cookie){
+        $arr_cookie_options = array (
+            // 'expires' => time() + 60*60*24*30, 
+            'path' => '/'//, 
+            // 'domain' => '.example.com', // leading dot for compatibility or use subdomain
+            // 'secure' => true,     // or false
+            // 'httponly' => true,    // or false
+            // 'samesite' => 'None' // None || Lax  || Strict
+            );
+        // setcookie('TestCookie', 'The Cookie Value', $arr_cookie_options);  
+        setcookie("admin_cookie", $admin_cookie, $arr_cookie_options);
+        if($debug) error_log("SA:_COOKIE=".print_r($_COOKIE,true));
+    }
+    header("Refresh:0; url=/");
+}
 
 ?>
