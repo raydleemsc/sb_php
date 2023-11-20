74ccfdfb5fa8d4880ed819ec1af03f2afaac60b1
Ray <raydlee.mobile@gmail.com>
2023-11-20 17:25:13 +0700

    update front for debugging

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
-// setcookie("admin_1698733138", "admin_1698733138");
+echo("<br>Index:_COOKIE=".print_r($_COOKIE,true));
+echo("<br>Index:_REQUEST=".print_r($_REQUEST,true));
 
 include("v8n/cc.php");
 $cc=cc_validate(true);
+echo("<br>Index:cc_validate=$cc");
 
 ?>
