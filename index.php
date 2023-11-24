183b17da8cce21d903bad2cefbafe7f8dbd04cfe
Ray <raydlee.mobile@gmail.com>
2023-11-24 11:52:57 +0700

    removed cc script for refactoring

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 echo("<br>Index:_COOKIE=".print_r($_COOKIE,true));
 echo("<br>Index:_REQUEST=".print_r($_REQUEST,true));
 
-include("v8n/cc.php");
-$cc=cc_validate(true);
-echo("<br>Index:cc_validate=$cc");
+include("v8n/validate.php");
+$v8n=validate(true);
+echo("<br>Index:validate=$v8n");
 
 ?>
--- v8n/cc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('validate.php');
-
-function cc_validate($debug=DEBUG_DEFAULT){
-    return validate($debug);
-}
-
-?>
