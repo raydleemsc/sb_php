bdd2a29f4a16ba0b3b0cd9ede5813d18517723ef
Raymond D. Lee, M. Sc <raydlee.msc@gmail.com>
2024-02-15 19:20:50 +0700

    updating

--- experiments/arrpos1.php
+++ experiments/arrpos1.php
@@ -8,9 +8,11 @@ $needles = array( ''
 );
 // $needles = implode(" ",$needles);
 $needles="local";
+echo("<br>needles=");
 var_dump($needles);
 
 $haystack = $_SERVER['SERVER_NAME'];
+echo("<br>haystack=");
 var_dump($haystack);
 
 include("fn_arrpos.php");
@@ -18,6 +20,6 @@ include("fn_arrpos.php");
 $found = arrpos($haystack,$needles);
 
 if($found>0)echo("found at ".$found);
-else echo("lost");
+else echo("<br>lost needle in haystack");
 
 ?>
