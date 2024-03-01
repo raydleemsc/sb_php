aa812d74974953299893a24fb6989dc500c072ad
Ray <raydlee.mobile@gmail.com>
2024-03-01 18:05:12 +0700

    developing currency table parsing

---
+++ curr_tab/show_tab.php
@@ -0,0 +1,15 @@
+<?php
+
+// read in the json
+// parse down to ctx and iso codes
+// sort by iso codes alpha descending
+// assign rc index by col then row
+// eg.
+// 01 11 21 31
+// 02 12 22 32
+// 03 13 23 33
+// etc
+// sort by rc
+// display in table
+
+?>
